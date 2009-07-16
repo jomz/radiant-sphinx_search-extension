b9430ece79ce560705333713d5371cc9a202fb7b
Josh French <josh@digitalpulp.com>
2009-07-16 18:16:33 -0400

    Add pagination to search results

---
+++ lib/sphinx_search/link_renderer.rb
@@ -0,0 +1,17 @@
+# Cribbed from Aissac's Ultrasphinx search extension, much obliged.
+module SphinxSearch
+  class LinkRenderer < WillPaginate::LinkRenderer
+    def initialize(tag, query)
+      @tag = tag
+      @query = query
+    end
+  
+    def page_link(page, text, attributes = {})
+      %Q{<a href="#{@tag.locals.page.url}?page=#{page}&query=#{@query}">#{text}</a>}
+    end
+
+    def page_span(page, text, attributes = {})
+      "<span class=\"page\">#{text}</span>"
+    end
+  end
+end
