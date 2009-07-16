a627ab2fcfe16e28ed84038ee98687e3f9d9beef
Josh French <josh@digitalpulp.com>
2009-07-16 14:36:09 -0400

    Initial SearchPage model and specs

---
+++ app/models/search_page.rb
@@ -0,0 +1,17 @@
+class SearchPage < Page
+  attr_accessor :results
+
+  def after_initialize
+    @results = []
+  end
+
+  def cache?
+    false
+  end
+
+  def process_with_search(request, response)
+    @results = Page.search(request.params[:query], :conditions => { :searchable => 1 })
+    process_without_search(request, response)
+  end
+  alias_method_chain :process, :search
+end
