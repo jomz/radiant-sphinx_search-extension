c69191b0ecf01d190179f4c9150ee197b979d561
Kunal Shah <kunal@digitalpulp.com>
2009-07-20 14:35:05 -0400

    Handle empty search terms

--- app/models/search_page.rb
+++ app/models/search_page.rb
@@ -13,7 +13,14 @@ class SearchPage < Page
     @request, @response = request, response
 
     @query = request.params[:query]
-    @results = Page.search(@query, :conditions => { :searchable => 1, :status_id => 100 }, :page => request.params[:page], :per_page => @@per_page)
+
+    # Validate only non-empty non-nil search terms
+    unless @query.blank?
+      @results = Page.search(@query, :conditions => { :searchable => 1, :status_id => 100 }, :page => request.params[:page], :per_page => @@per_page)
+    else
+      # Stub the expected collection object
+      @results = ThinkingSphinx::Collection.new(0,@@per_page,0,0)
+    end
 
     super(request, response)
   end
