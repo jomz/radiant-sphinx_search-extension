a725bc55df85edb114647feb107d0a9e06d6db40
Josh French <josh@digitalpulp.com>
2009-07-21 17:42:47 -0400

    Kaizen

--- app/models/search_page.rb
+++ app/models/search_page.rb
@@ -10,18 +10,10 @@ class SearchPage < Page
   end
 
   def process(request, response)
-    @request, @response = request, response
-
     @query = request.params[:query]
-
-    # Validate only non-empty non-nil search terms
-    unless @query.blank?
-      @results = Page.search(@query, :conditions => { :searchable => 1, :status_id => 100 }, :page => request.params[:page], :per_page => @@per_page)
-    else
-      # Stub the expected collection object
-      @results = ThinkingSphinx::Collection.new(0,@@per_page,0,0)
-    end
-
-    super(request, response)
+    # Ensure search is not run with empty term
+    @results = @query.blank? ? ThinkingSphinx::Collection.new(0,@@per_page,0,0) :
+                               Page.search(@query, :conditions => { :searchable => 1, :status_id => 100 }, :page => request.params[:page], :per_page => @@per_page)
+    super
   end
 end
