0c3251fdfa5e05b3d31b18237da7dab77e8724c6
Josh French <josh@digitalpulp.com>
2010-08-08 14:39:58 -0400

    Expose group_concat_max

--- lib/sphinx_search/page_extensions.rb
+++ lib/sphinx_search/page_extensions.rb
@@ -5,7 +5,7 @@ module SphinxSearch
    
    def self.included(base)
      base.define_index do
-       set_property :delta => true, :group_concat_max_len => 8.kilobytes
+       set_property :delta => true, :group_concat_max_len => Radiant::Config['search.content_length'] || 8.kilobytes
        set_property :field_weights => { 'title' => 100 }
        indexes title, parts.content
        has updated_at, status_id, virtual, class_name
