1b1ad13b23cb128a3c9b15ff385964189ac1016d
Josh French <josh@digitalpulp.com>
2010-08-08 17:46:33 -0400

    Set defaults (mostly for dev mode safety)

--- lib/sphinx_search.rb
+++ lib/sphinx_search.rb
@@ -1,3 +1,6 @@
 module SphinxSearch
   mattr_accessor :param_name, :content_length, :hidden_classes
+  self.param_name = 'q'
+  self.content_length = 8.kilobytes
+  self.hidden_classes = %w(SearchPage JavascriptPage StylesheetPage)
 end
